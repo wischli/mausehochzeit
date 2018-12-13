40433b004b8ae658d6f1bcb7ff064182738472ff
William <william@monstertipp.com>
2018-12-13 14:34:14 +0100

    rename and improve Menu

---
+++ src/assets/scripts/Menu.js
@@ -0,0 +1,87 @@
+import { stack as Menu } from 'react-burger-menu';
+import React from 'react';
+import icon from '../../images/mouse-black-animal.svg';
+import '../styles/Menu.css';
+import { FormatListNumbered, Schedule, Palette, LocalHotel, Place, Info, AddComment } from '@material-ui/icons';
+
+class MyMenu extends React.Component {
+  constructor(props){
+    super(props);
+    this.state = {
+      menuItems : [
+        {
+          href : '#survey',
+          title : 'Umfrage',
+          icon: <FormatListNumbered />
+        },
+        {
+          href : '#location',
+          title : 'Die Location',
+          icon: <Place />
+        },
+        {
+          href : '#dosDonts',
+          title : 'Zu beachten',
+          icon: <Info />
+        },
+        {
+          href : '#timeline',
+          title : 'Ablauf',
+          icon: <Schedule />
+        },
+        {
+          href : '#colors',
+          title : 'Kleidung und Farben',
+          icon: <Palette />
+        },
+        {
+          href : '#program',
+          title : 'Programmideen',
+          icon: <AddComment />
+        },
+        {
+          href : '#unterkunft',
+          title : 'Schlafen in der Umgebung',
+          icon: <LocalHotel />
+        },
+      ],
+      isOpen : false,
+      menuOpen: false,
+    };
+  }
+
+  renderMenuItems = (state) => {
+    return this.state.menuItems.map(item => {
+      return <a onClick={() => this.closeMenu()} className="menu-item" key={item.title} href={item.href}>{item.icon ? item.icon : ''}<div className="menu-text" style={{flex: 1}}>{item.title}</div></a>;
+    });
+  };
+
+  handleStateChange (state) {
+    this.setState({menuOpen: state.isOpen})
+  }
+
+  closeMenu () {
+    this.setState({menuOpen: false})
+  }
+
+  toggleMenu () {
+    this.setState({menuOpen: !this.state.menuOpen})
+  }
+
+  render () {
+    return (
+      <Menu
+        isOpen = {this.state.menuOpen}
+        onStateChange={ (state) => this.handleStateChange(state) }
+        customBurgerIcon={ <img src={icon} alt="Menu" /> }
+        burgerButtonClassName={ this.state.menuOpen ? 'menuOpen' : '' }
+        pageWrapId={ "menuPageWrap" }
+        outerContainerId={ "menuOuterContainer" }>
+        {this.renderMenuItems()}
+          <div onClick={this.isMenuOpe} className="menu-header">Menu</div>
+      </Menu>
+    );
+  }
+}
+
+export default MyMenu;
