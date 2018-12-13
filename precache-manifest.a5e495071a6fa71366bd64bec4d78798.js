4bb420a0c4cbf9e11c0dbea3d451285f25a0de77
William <william@monstertipp.com>
2018-12-13 15:59:10 +0100

    Updates

---
+++ precache-manifest.a5e495071a6fa71366bd64bec4d78798.js
@@ -0,0 +1,78 @@
+self.__precacheManifest = [
+  {
+    "revision": "2a2a3ec20ee36add3768dc5b20904084",
+    "url": "/static/media/bellevue-review.2a2a3ec2.png"
+  },
+  {
+    "revision": "a007a5d159e93aeda65c",
+    "url": "/static/css/main.ecb26b1b.chunk.css"
+  },
+  {
+    "revision": "23f373a9300b048de5a6",
+    "url": "/static/js/1.23f373a9.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "8c93fd97b714f2d044175096c661db03",
+    "url": "/static/media/mouse-black-animal.8c93fd97.svg"
+  },
+  {
+    "revision": "a7cbd83a895f44dbadbdcf820662def0",
+    "url": "/static/media/MausehochzeitTeaser_mobile_2.a7cbd83a.jpg"
+  },
+  {
+    "revision": "be9228a65050b59f0279f34c52cfe0da",
+    "url": "/static/media/MausehochzeitTeaser_desktop.be9228a6.jpg"
+  },
+  {
+    "revision": "ab5ad022f9494179ec6c1718e28b86c7",
+    "url": "/static/media/StadtscheuneBuckow.ab5ad022.jpg"
+  },
+  {
+    "revision": "58a653992229cc1913677bc196a5d954",
+    "url": "/static/media/StadtscheuneBuckow2.58a65399.jpg"
+  },
+  {
+    "revision": "a007a5d159e93aeda65c",
+    "url": "/static/js/main.a007a5d1.chunk.js"
+  },
+  {
+    "revision": "122f92424800cd4aef58915981671888",
+    "url": "/static/media/bergschloesschen-review.122f9242.png"
+  },
+  {
+    "revision": "0e36ac880b361ac94344e246a0a57789",
+    "url": "/static/media/strandhotel-buckow-review.0e36ac88.png"
+  },
+  {
+    "revision": "112479bd6141e8297061aa6160ef464c",
+    "url": "/static/media/vier-jahreszeiten-review.112479bd.png"
+  },
+  {
+    "revision": "5b6d1726d56e944667fb642bdec3635c",
+    "url": "/static/media/stobbermuehle-review.5b6d1726.png"
+  },
+  {
+    "revision": "a2e5ce4e25c01b5d703c0c085f75ace1",
+    "url": "/static/media/hotel-maerkische-schweiz-review.a2e5ce4e.png"
+  },
+  {
+    "revision": "af532ecc0a8f90ee9563193302ab81df",
+    "url": "/static/media/LaserMetal.af532ecc.ttf"
+  },
+  {
+    "revision": "01782a7fff27bc527f8af8095aac1098",
+    "url": "/static/media/ShadowsIntoLight.01782a7f.ttf"
+  },
+  {
+    "revision": "565eef624f2302aeb70e496ac4c366b1",
+    "url": "/static/media/sunday.565eef62.ttf"
+  },
+  {
+    "revision": "bc0b482b79ea21bb89e05283989a9090",
+    "url": "/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "mausehochzeit2019.de/precache-manifest.595890d5ae43d322ac403575e66cb266.js"
+  "/precache-manifest.a5e495071a6fa71366bd64bec4d78798.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("mausehochzeit2019.de/index.html", {
+workbox.routing.registerNavigationRoute("/index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
