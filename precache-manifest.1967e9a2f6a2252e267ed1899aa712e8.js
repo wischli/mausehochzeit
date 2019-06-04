baa927cb97b5f2b35af68ca9184ec6c34908f61e
William <william@monstertipp.com>
2019-06-04 11:02:58 +0200

    Updates

---
+++ precache-manifest.1967e9a2f6a2252e267ed1899aa712e8.js
@@ -0,0 +1,78 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "608e48f8cd3bbde1a212eccb3016adf6",
+    "url": "/index.html"
+  },
+  {
+    "revision": "613fa9ef699f8315f656",
+    "url": "/static/css/main.b023aafc.chunk.css"
+  },
+  {
+    "revision": "df8aad9d7901eeedd57c",
+    "url": "/static/js/2.ed1013f3.chunk.js"
+  },
+  {
+    "revision": "613fa9ef699f8315f656",
+    "url": "/static/js/main.7e332522.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "af532ecc0a8f90ee9563193302ab81df",
+    "url": "/static/media/LaserMetal.af532ecc.ttf"
+  },
+  {
+    "revision": "be9228a65050b59f0279f34c52cfe0da",
+    "url": "/static/media/MausehochzeitTeaser_desktop.be9228a6.jpg"
+  },
+  {
+    "revision": "a7cbd83a895f44dbadbdcf820662def0",
+    "url": "/static/media/MausehochzeitTeaser_mobile_2.a7cbd83a.jpg"
+  },
+  {
+    "revision": "01782a7fff27bc527f8af8095aac1098",
+    "url": "/static/media/ShadowsIntoLight.01782a7f.ttf"
+  },
+  {
+    "revision": "ab5ad022f9494179ec6c1718e28b86c7",
+    "url": "/static/media/StadtscheuneBuckow.ab5ad022.jpg"
+  },
+  {
+    "revision": "58a653992229cc1913677bc196a5d954",
+    "url": "/static/media/StadtscheuneBuckow2.58a65399.jpg"
+  },
+  {
+    "revision": "2a2a3ec20ee36add3768dc5b20904084",
+    "url": "/static/media/bellevue-review.2a2a3ec2.png"
+  },
+  {
+    "revision": "122f92424800cd4aef58915981671888",
+    "url": "/static/media/bergschloesschen-review.122f9242.png"
+  },
+  {
+    "revision": "a2e5ce4e25c01b5d703c0c085f75ace1",
+    "url": "/static/media/hotel-maerkische-schweiz-review.a2e5ce4e.png"
+  },
+  {
+    "revision": "8c93fd97b714f2d044175096c661db03",
+    "url": "/static/media/mouse-black-animal.8c93fd97.svg"
+  },
+  {
+    "revision": "5b6d1726d56e944667fb642bdec3635c",
+    "url": "/static/media/stobbermuehle-review.5b6d1726.png"
+  },
+  {
+    "revision": "0e36ac880b361ac94344e246a0a57789",
+    "url": "/static/media/strandhotel-buckow-review.0e36ac88.png"
+  },
+  {
+    "revision": "565eef624f2302aeb70e496ac4c366b1",
+    "url": "/static/media/sunday.565eef62.ttf"
+  },
+  {
+    "revision": "112479bd6141e8297061aa6160ef464c",
+    "url": "/static/media/vier-jahreszeiten-review.112479bd.png"
+  }
+]);
